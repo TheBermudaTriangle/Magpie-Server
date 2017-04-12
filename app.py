c4567c4756cc41b1a4e5138262d8075c20e35a95
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-04-13 00:08:35 +0530

    initial commit

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ mylist = []
 mylist = [{username:'nit',lat:29.951573,lng:76.815305},{username:'singhal',lat:26.830996,lng:75.762118},{username:'swapnil',lat:25.5794912,lng:85.1167952},{username:'bhatiya',lat:28.3953089,lng:77.2698073}]
 class TodoList(Resource):
     def get(self):
-        return {data:mylist}
+        return {"data":mylist}
 
     def post(self):
         parser = reqparse.RequestParser()
-		parser.add_argument('lat')
-		parser.add_argument('lng')
-		args = parser.parse_args()
-		lat = args['lat']
-		lon = args['lng']
-		myDict[]
-        return {data:mylist}
+        parser.add_argument('lat')
+        parser.add_argument('lng')
+        args = parser.parse_args()
+        lat = args['lat']
+        lon = args['lng']
+        myDict[]
+        return {"data":mylist}
 
 ##
 ## Actually setup the Api resource routing here
