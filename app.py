b285197d38b1b22b1bb574c0f1c9c919addebdf4
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-04-13 00:06:39 +0530

    initial commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-var mylist = []
+mylist = []
 
 mylist = [{username:'nit',lat:29.951573,lng:76.815305},{username:'singhal',lat:26.830996,lng:75.762118},{username:'swapnil',lat:25.5794912,lng:85.1167952},{username:'bhatiya',lat:28.3953089,lng:77.2698073}]
 class TodoList(Resource):
@@ -19,8 +19,8 @@ class TodoList(Resource):
 		parser.add_argument('lat')
 		parser.add_argument('lng')
 		args = parser.parse_args()
-		var lat = args['lat']
-		var lon = args['lng']
+		lat = args['lat']
+		lon = args['lng']
 		myDict[]
         return {data:mylist}
 
